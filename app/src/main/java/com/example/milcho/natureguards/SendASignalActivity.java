ef876e5be9f48650f4bd8ed7e5d65765d4932c55
Lilly Mihaylova <lillymihailoa@abv.bg>
2016-04-16 10:13:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/milcho/natureguards/SendASignalActivity.java
@@ -0,0 +1,19 @@
+package com.example.milcho.natureguards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SendASignalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_asignal);
+
+
+
+    }
+
+
+}
